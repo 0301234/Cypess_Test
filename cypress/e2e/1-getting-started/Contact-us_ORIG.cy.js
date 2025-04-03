62a8ba27e958386fd58148b768809e1b51015906
Muhammad Zeeshan <99110274+0301234@users.noreply.github.com>
2025-04-03 16:22:42 +0500

    Update Contact-us_ORIG.cy.js
    from GitHuB

--- cypress/e2e/1-getting-started/Contact-us_ORIG.cy.js
+++ cypress/e2e/1-getting-started/Contact-us_ORIG.cy.js
@@ -5,6 +5,7 @@ describe("Test Contact Us form via Automation Test Store", () => {
         cy.visit("https://www.automationteststore.com/");
         cy.get("a[href$='contact']").click();
         //cy.xpath("//a[contains(@href, 'contact')]").click(joe);
+        // this is from GitHub
         cy.get('#ContactUsFrm_first_name').type("joe");
         cy.get('#ContactUsFrm_email').type("joe_blogs123@gmail.com");
         cy.get('#ContactUsFrm_enquiry').type("Do you provide additional discount on bulk orders?")

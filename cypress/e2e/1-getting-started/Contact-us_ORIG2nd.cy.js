29c1c99d34ebef585a6f3a8a5ff9e7434ca0ac9b
0301234 <zishanmushtaq@gamil.com>
2025-04-04 17:09:18 +0500

    6th

---
+++ cypress/e2e/1-getting-started/Contact-us_ORIG2nd.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe("Test Contact Us form via Automation Test Store", () => {
+    it("Should be able to submit a successful submission via contact us form", () => {
+        cy.visit("https://www.automationteststore.com/");
+        cy.get("a[href$='contact']").click();
+        cy.get('#ContactUsFrm_first_name').type("joe");
+        cy.get('#ContactUsFrm_email').type("joe_blogs123@gmail.com");
+        cy.get('#ContactUsFrm_enquiry').type("Do you provide additional discount on bulk orders?")
+        cy.get("button[title='Submit']").click();
+    });
+})
